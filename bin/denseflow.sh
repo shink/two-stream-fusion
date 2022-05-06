a60c0ef2b0e514fc623ca9e6ccb573d1ac6fcee8
Yuanhao Ji <jiyuanhao@apache.org>
2022-05-06 11:36:08 +0800

    finish denseflow shell
    issue: #2

--- bin/denseflow.sh
+++ bin/denseflow.sh
@@ -6,20 +6,22 @@ dataset_path=$1
 # the video list path
 video_list_path=$2
 
-cd "$dataset_path" || exit
-mkdir result
+# the result path
+result_path=$3
 
-OLDIFS=$IFS
-IFS=','
+# number of GPUs
+num_gpu=$4
 
+[ ! -f "$dataset_path" ] && echo "$dataset_path directory not found" && exit
 [ ! -f "$video_list_path" ] && echo "$video_list_path file not found" && exit
+[ ! -f "$result_path" ] && echo "$result_path directory not found" && exit
 
-while read -r label video_path; do
-  echo "label: $label"
-  echo "path: $video_path"
+sed -i "s/\$DATASET_PATH/\/data/g" "$video_list_path"
 
-done <"$video_list_path"
-
-IFS=$OLDIFS
-
-#sudo docker run --gpus all -it -v "$dataset_path":/data wizyoung/optical-flow-gpu bash -c
+sudo docker run --gpus all -it --rm \
+                -v "$dataset_path":/data \
+                -v "$video_list_path":/video_list.txt \
+                -v "$result_path":/result \
+                --workdir=/src \
+                wizyoung/optical-flow-gpu \
+                bash -c "python multi_gpu_extract.py /video_list.txt /result --flow_type tvl1 --out_fmt dir --num_gpu $num_gpu --step 1 --keep_frames True --log /data/log.log"
